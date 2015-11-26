0033846358db15980931c61d3f2bf3f87595697e
Jorge Maria <orck10@gmail.com>
2015-11-26 20:12:49 -0200

    Update Animal.java

--- animal/src/animal/Animal.java
+++ animal/src/animal/Animal.java
@@ -1,20 +1,38 @@
 package animal;
 
-public class Animal {
+public class Animal implements Comparable<Animal>{
+	public String nome;
 	public String raca;
 	public double peso;
 	public int idade;
 	
-	public Animal(String raca, double peso, int idade) {
+	public Animal(String nome, String raca, double peso, int idade) {
 		super();
+		this.nome = nome;
 		this.raca = raca;
 		this.peso = peso;
 		this.idade = idade;
 	}
 	
-
 	public String fazerBarulho(){
 		return "Animal faz barulho";
 	}
+	
+	@Override
+	public String toString()
+	{
+		return "Nome: "+nome +" Raça: "+raca + " Peso: " + peso + " Idade: " + idade;
+	}
+	
+	@Override
+	public int compareTo(Animal o)
+	{
+		if(this.peso < o.peso)
+			return -1;
+		if(this.peso > o.peso)
+			return 1;
+		return 0;
+	}
 
 }
+

415b1d691747c09ca594b5d6a6efe7c752cc15da
Jorge Maria <orck10@gmail.com>
2015-11-26 20:26:54 -0200

    Update Main.java

--- animal/src/animal/Main.java
+++ animal/src/animal/Main.java
@@ -1,18 +1,35 @@
 package animal;
 
+import java.util.Collections;
+
 public class Main {
 
 	public static void main(String[] args) {
 		Manada mana = new ManadaVirgula();
 		Manada mana2 = new ManadaSustenido();
 		
-		mana.adicionarAnimal(new Gato("Persa", 7.5, 2));
-		mana.adicionarAnimal(new Cachorro("Bull Dog", 15.4, 3));
-		mana.adicionarAnimal(new Animal("Mangalarga", 150.0, 5));
+		mana.adicionarAnimal(new Gato("Manda Chuva","Persa", 7.5, 2));
+		mana.adicionarAnimal(new Cachorro("Xuxu","Bull Dog", 15.4, 3));
+		mana.adicionarAnimal(new Animal("Batatinha","Mangalarga", 150.0, 5));
 		
-		mana2.adicionarAnimal(new Gato("Ragdoll", 5.5, 2));
-		mana2.adicionarAnimal(new Cachorro("Pastor Canadense", 15.4, 3));
-		mana2.adicionarAnimal(new Animal("Shire", 160.0, 5));
+		mana2.adicionarAnimal(new Cachorro("Betovem","Pastor Canadense", 15.4, 3));
+		mana2.adicionarAnimal(new Gato("Lecie","Ragdoll", 5.5, 2));
+		mana2.adicionarAnimal(new Animal("Napoleão","Shire", 16.0, 5));
+		
+		System.out.println("\n Ordenar por peso\n");
+		Collections.sort(mana2.animais);
+		for(Animal a : mana2.animais)
+		{
+			System.out.println(a+"\n");
+		}
+		
+		System.out.println("\n Ordenar por nome\n");
+		CompararNome nomes = new CompararNome();
+		Collections.sort(mana.animais,nomes);
+		for(Animal a: mana.animais)
+		{
+			System.out.println(a+"\n");
+		}
 		
 		System.out.println(mana.estourar());
 		
@@ -20,3 +37,4 @@ public class Main {
 	}
 
 }
+
